fc26600a43a15cd96c14447420478fac4edf7708
sarahjoseph723 <sjoseph@stuy.edu>
2016-03-07 13:12:21 -0500

    .

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+var c =  document.getElementById("slate");
+var ctx = c.getContext("2d");
+
+var frameid;
+
 //start button that draws a semicircle
+var startB = function() {
+    //code to draw semicircle
+};
+
+var start = document.getElementById("s");
+s.addEventListener("click", startB);
 
 //function that reacts to key pressed
 
