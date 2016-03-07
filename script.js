94bc0b0bd3a8f4b1c53c8acb60ac448535423bee
Shi Shu <shishu_1998@yahoo.com>
2016-03-07 13:07:55 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,9 @@
+//start button that draws a semicircle
+
+//function that reacts to key pressed
+
+//function that drops objects from the sky
+
+//function that subtracts a life when an object falls to the bottom and checks if lives = 0
+
+//function that catches and adds a point when an object is picked up
