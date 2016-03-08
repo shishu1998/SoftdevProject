ed2249a9c82127896c5cb18ad6a50d1aac1d0b95
shishu1998 <shishu_1998@yahoo.com>
2016-03-07 21:29:38 -0500

    move

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ var c =  document.getElementById("slate");
 var ctx = c.getContext("2d");
 
 //x-coordinate of the semicircle
-var sx;
+var sx = c.width/2;
 var frameid;
 
 //start button that draws a semicircle
 var startB = function() {
     //code to draw semicircle
-    sx = c.width/2;
+    ctx.clearRect(0,0,c.width,c.height);
     ctx.beginPath();
     ctx.arc(sx,480,20,0,Math.PI);
-    
+    ctx.stroke();
 };
+
+var move = function(e){
+    console.log(e);
+    if(sx != 20 && sx != c.width-20){
+	console.log(sx);
+	sx = sx + 10;	
+	startB();
+    }
+};
+
+//c.addEventListener("keydown",move);
 
 var start = document.getElementById("s");
 s.addEventListener("click", startB);
