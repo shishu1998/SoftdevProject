c1cc491c80a7b07b667c374dec338c4e5a5f18cc
Dalton Wu <dalton.z.wu@gmail.com>
2016-03-10 21:36:43 -0500

    Update

--- script.js
+++ script.js
@@ -1,73 +1,159 @@
-var c =  document.getElementById("slate");
-var ctx = c.getContext("2d");
-var scoreboard = document.getElementById("score");
-var score = 0;
+/*
+DW Softdev, Term 2, Spring 2016
+Shi Shu, Sarah Joseph, Dalton Wu
 
-//x-coordinate of the semicircle
-var sx = c.width/2;
-var frameid;
+CATCHER (HTML canvas game)
+ */
 
-//start button that draws a semicircle, attaches eventlistener
-var startB = function() {
-    //code to draw semicircle
-    window.addEventListener("keydown",move);
-    draw();
-};
+// variables displayed on the HTML page
+var score;
+var level;
 
+// time-storing variables [milliseconds]
+var lastLeveUp;
+var lastPancakeUp;
+var pancakeCooldown;
 
-var move = function(e){
-    console.log(e);
-    if(e.keyIdentifier == "Left" && sx != 40){
-	sx = sx - 5;	
-    }
-    if(e.keyIdentifier == "Right" && sx != c.width){
-	sx = sx + 5;
-    }
-};
+var things; // holds all the things to draw; index 0 is the catcher
+var frameId = 0;
+var triggered;
 
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var bound = canvas.getBoundingClientRect();
 
-var start = document.getElementById("s");
-s.addEventListener("click", startB);
-//function that reacts to key pressed
+function Catcher() {
+	// rectangular basket
+	
+	// the parameters used in fillRect()
+	this.width = 100;
+	this.height = 50;
+	this.x = canvas.width/2 - this.width/2;
+	this.y = canvas.height - 2*this.height;
+	
+	this.draw = function() {
+		ctx.beginPath();
+		ctx.fillStyle = "#ea4"; // nice baskety color
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 0.5;
+		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.stroke();
+		ctx.fill();
+	};
+	
+	this.moveTo = function(mouseX, mouseY) {
+		// compensate for position of canvas boundingRect
+		this.x = mouseX - this.width/2 - bound.left;
+		this.y = mouseY - this.height/2 - bound.top;
+	};
+}
 
-//function that drops objects from the sky 
+function Pancake() {
+	// pancake of random pastel color o_0
+	
+	// the parameters used in fillRect()
+	this.width = 42;
+	this.height = 5;
+	this.x = Math.floor(Math.random() * (canvas.width - this.width + 1));
+	this.y = 0;
+	
+	// random pastel color (rgb)
+	var foo = [];
+	var i;
+	for(i = 0; i < 3; i++) {
+		foo.push(Math.floor(Math.floor((Math.random() * 256 + 255) / 2)));
+	}
+	this.color = "rgb(" + foo[0] + ", " + foo[1] + ", " + foo[2] + ")";	
+	
+	this.draw = function() {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 0.5;
+		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.stroke();
+		ctx.fill()
+	};
+}
 
-ctx.fillStyle = "#FF0000";
-var dropid;
+function EXPLODE() {
+	window.open("failure.html", "_self");
+	setTimeout(function() {
+		window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_self");
+	}, 500);
+}
 
-var draw = function(){
-    var radius = 10;
-    var xcor = 250;
-    var ycor = 0;
-    
-    var animCode = function() {
-	console.log("ok");
-
-        if (ycor == c.height-10) {
-	    console.log(ycor);
-	    window.cancelAnimationFrame(dropid);
+function move(time) {
+	// does the game o_O
+	
+	if(time - lastLevelUp > 5000) {
+		level *= 2;
+		pancakeCooldown /= 2;
+		lastLevelUp = time;
+		document.getElementById("level").innerHTML = level.toString();
+	}
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	// adds more pancakes
+	if(things.length <= 1 || (things.length - 1 < level
+	   && time - lastPancakeUp > pancakeCooldown)) {
+		things.push(new Pancake());
+		lastPancakeUp = time;
+	}
+	
+	var i = 1;
+	while(i < things.length) {
+		var bottomY = things[i].y + things[i].height;
+		if(bottomY >= things[0].y) {
+			if(things[i].x >= things[0].x
+			   && things[i].x + things[i].width <= things[0].x + things[0].width) {
+				things.splice(i, 1);
+				score++;
+				document.getElementById("score").innerHTML = score.toString();
+			}
+			else {
+				triggered = true;
+				break;
+			}
+		}
+		else if(bottomY >= canvas.height) {
+			triggered = true;
+			break;
+		}
+		else {
+			things[i].y += level > 5 ? 5 : level;
+			things[i].draw();
+			i++;
+		}
+	}
+	
+	// must be drawn last because front of basket is in front of pancakes
+	things[0].draw();
+	
+	if(triggered) {
+		EXPLODE();
 	}
 	else {
-	    ctx.clearRect(0,0,c.width,c.height);
-	    ctx.beginPath();
-	    ctx.arc(xcor, ycor, radius, 0, 2*Math.PI);
-	    ctx.fill();
-	    ctx.moveTo(sx,480);
-	    ctx.arc(sx - 20,480,20,0,Math.PI);
-	    ctx.stroke();
-	    dropid = window.requestAnimationFrame(animCode);
-	    ycor ++;
+		frameId = window.requestAnimationFrame(move);
 	}
-    };
+}
 
-    animCode();
-};
-
-var changescore = function(){
-    score ++;
-    scoreboard.value = score;
-};
-
-//function that subtracts a life when an object falls to the bottom and checks if lives = 0
-
-//function that catches and adds a point when an object is picked up
+function start() {
+	// initialization and reset
+	
+	level = 1;
+	document.getElementById("level").innerHTML = level.toString();
+	score = 0;
+	things = [new Catcher(), new Pancake()];
+	lastLevelUp = performance.now();
+	lastPancakeUp = performance.now();
+	pancakeCooldown = 5000;
+	triggered = false;
+	
+	canvas.addEventListener("mousemove", function(e) {
+		things[0].moveTo(e.clientX, e.clientY);
+	});
+	
+	move();
+}
